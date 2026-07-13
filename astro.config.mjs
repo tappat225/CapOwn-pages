0f2fde445bd6827bcadef7150cd8c4b8d2b692a9
tappat <tappat@vip.qq.com>
2026-07-13 15:32:32 +0800

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-  site: 'https://www.capown.net',
+  site: isGitHubPages
+    ? 'https://tappat225.github.io'
+    : 'https://www.capown.net',
+  base: isGitHubPages ? '/CapOwn-pages' : '/',
   output: 'static',
   trailingSlash: 'always',
   build: {
